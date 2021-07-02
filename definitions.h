5941c54e01c4c38bcccd03e393d478fc2a868e83
alicedb2 <alice.doucet.beaupre@gmail.com>
2021-07-01 21:52:00 -0500

    scale and rotation pin

--- definitions.h
+++ definitions.h
@@ -1,65 +1,123 @@
-#define SETEUCL1_PIN A0
-#define SETEUCL2_PIN A1
-#define SETEUCL1SHADOW_PIN A6
-#define SETEUCL2SHADOW_PIN A7
+#define SETEUCL_PIN A0
+#define SETEUCLSHADOW_PIN A6
+#define SETROTATION_PIN A1
+#define SETSCALE_PIN A2
 
 #define BUTTON_PIN 3
 #define CLOCKIN_PIN 2
-#define CVOUT1_PIN 10
-#define CVOUT2_PIN 6
-#define GATE1_PIN A4
-#define GATE2_PIN A5
+#define CVOUT_PIN 10
+#define GATE_PIN A3
 
 #define EMPTY_CHAR ' '
 #define EMPTY_LINE "                "
 
-#define EUCL1_COLUMN 1
-#define EUCL2_COLUMN 2
+#define EUCL_COLUMN 1
 
-// Tuning sucks. 
-// Use a voltmeter and measure voltage
-// between pole of the capacitor of the
-// low pass filter and fiddle with the
-// x value passed to analogWrite(CVOUT1_PIN, x)
-// to get as close to 1/12 mV per semitone.
-// Triple check your x's.
-uint8_t semitone_cvs_1[] = {
-  0,  // 0.00 mV C0
-  6,  // 83 mV
-  12, // 167 mV
-  18, // 250 mV
-  24, // 333 mV
-  30, // 417 mV
-  36, // 500 mV
-  42, // 583 mV
-  49, // 667 mV
-  56, // 750 mV
-  62, // 833 mV
-  68, // 917 0 mV
-  73, // 1000 mV C1
-  78, // 1083 mV
-  83, // 1167 mV
-  88 // 1250 mV
+const uint16_t semitone_cvs_dac[57] = {
+  6, // C2
+  79,
+  151,
+  224,
+  297,
+  368,
+  441,
+  509,
+  584,
+  653,
+  727,
+  799,
+  870, // C3
+  944,
+  1014,
+  1087,
+  1157,
+  1232,
+  1302,
+  1374,
+  1446,
+  1516,
+  1588,
+  1658,
+  1733, // C4
+  1802,
+  1876,
+  1946,
+  2018,
+  2091,
+  2162,
+  2237,
+  2306,
+  2381,
+  2452,
+  2525,
+  2595, // C5
+  2667,
+  2741,
+  2810,
+  2884,
+  2954,
+  3027,
+  3096,
+  3168,
+  3240,
+  3311,
+  3385,
+  3453, // C6
+  3528,
+  3596,
+  3669,
+  3739,
+  3812,
+  3884,
+  3954,
+  4030  // A6
 };
 
-uint8_t semitone_cvs_2[] {
-  0,  // 0.00 mV C0
-  5,  // 83 mV
-  11, // 167 mV
-  17, // 250 mV
-  22, // 333 mV
-  28, // 417 mV
-  34, // 500 mV
-  40, // 583 mV
-  47, // 667 mV
-  53, // 750 mV
-  59, // 833 mV
-  65, // 917 0 mV
-  71, // 1000 mV C1
-  76,
-  81,
-  85
-};
+
+//// Tuning sucks.
+//// Use a voltmeter and measure voltage
+//// between pole of the capacitor of the
+//// low pass filter and fiddle with the
+//// x value passed to analogWrite(CVOUT1_PIN, x)
+//// to get as close to 1/12 mV per semitone.
+//// Triple check your x's.
+//uint8_t semitone_cvs_1[] = {
+//  0,  // 0.00 mV C2
+//  6,  // 83 mV
+//  12, // 167 mV
+//  18, // 250 mV
+//  24, // 333 mV
+//  30, // 417 mV
+//  36, // 500 mV
+//  42, // 583 mV
+//  49, // 667 mV
+//  56, // 750 mV
+//  62, // 833 mV
+//  68, // 917 0 mV
+//  73, // 1000 mV C3
+//  78, // 1083 mV
+//  83, // 1167 mV
+//  88 // 1250 mV
+//};
+//
+//uint8_t semitone_cvs_2[] {
+//  0,  // 0.00 mV C0
+//  5,  // 83 mV
+//  11, // 167 mV
+//  17, // 250 mV
+//  22, // 333 mV
+//  28, // 417 mV
+//  34, // 500 mV
+//  40, // 583 mV
+//  47, // 667 mV
+//  53, // 750 mV
+//  59, // 833 mV
+//  65, // 917 0 mV
+//  71, // 1000 mV C1
+//  76,
+//  81,
+//  85
+//};
 
 byte Filled_lowo[8] =
 {
@@ -209,7 +267,7 @@ const uint16_t scales[] = {
   HARMONIC_MAJOR, // 90
   DOUBLE_HARMONIC_MAJOR,  // 91
   NEAPOLITAN_MAJOR, // 92
-  MAJOR_LOCARIAN, // 93
+  MAJOR_LOCRIAN, // 93
   BEBOP_MAJOR,  // 94
   HEXATONIC_1_MAJOR,  // 95
   HEXATONIC_2_MAJOR,  // 96
@@ -220,11 +278,30 @@ const uint16_t scales[] = {
   MINOR_MAJOR,  // 101
   DOUBLE_HARMONIC_MINOR,  // 102
   NEAPOLITAIN_MINOR,  // 103
-  MINOR_LOCARIAN, // 104
+  MINOR_LOCRIAN, // 104
   BEBOP_MINOR,  // 105
   HEXATONIC_1_MINOR,  // 106
   HEXATONIC_2_MINOR,  // 107
   PENTATONIC_1_MINOR, // 108
   PENTATONIC_2_MINOR, // 109
   PENTATONIC_3_MINOR  // 110
+};
+
+
+const char key_C[2] = "C";
+const char key_Csharp[6] = "C#";
+const char key_D[2] = "D";
+const char key_Dsharp[6] = "D#";
+const char key_E[2] = "E";
+const char key_F[2] = "F";
+const char key_Fsharp[6] = "F#";
+const char key_G[2] = "G";
+const char key_Gsharp[6] = "G#";
+const char key_A[2] = "A";
+const char key_Asharp[6] = "A#";
+const char key_B[2] = "B";
+
+const char *const notes[12] = {
+  key_C, key_Csharp, key_D, key_Dsharp, key_E, key_F,
+  key_Fsharp, key_G, key_Gsharp, key_A, key_Asharp, key_B
 };
